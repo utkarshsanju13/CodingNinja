7e6baa1ef75a29db000037e546ee4c3dfa9a929c
utkarshsanju13 <utkarsh.gupta01@nagarro.com>
2023-03-26 19:50:55 +0530

    initial Commit

--- src/array_1/arrangeNumberInArray.java
+++ src/array_1/arrangeNumberInArray.java
@@ -4,6 +4,7 @@ package array_1;
 from the user will be N and you need not worry about the array.
 Your task is to populate the array using the integer values in the
 range 1 to N(both inclusive) in the order - 1,3,5,.......,6,4,2.
+
 */
 
 public class arrangeNumberInArray {
